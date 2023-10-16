5f94246932a6dd8224e5e70605bc22c63309c9ab
Daniil <koldyrev98@icloud.com>
2023-10-16 17:07:08 +0300

    Add 3 lab

--- 2lab/2lab/2lab.cpp
+++ 2lab/2lab/2lab.cpp
@@ -9,38 +9,49 @@
 
 struct Params
 {
-    Bitmap* in;
-    uint32_t startHeight;
-    uint32_t endHeight;
-    uint32_t startWidth;
-    uint32_t endWidth;
+    Bitmap* in;         // Указатель на изображение
+    uint32_t startHeight; // Начальная высота области для размытия
+    uint32_t endHeight;   // Конечная высота области для размытия
+    uint32_t startWidth;  // Начальная ширина области для размытия
+    uint32_t endWidth;    // Конечная ширина области для размытия
 };
 
+// Функция Blur выполняет размытие изображения
 void Blur(int radius, Params* params)
 {
+    // Вычисление нескольких параметров для размытия
     float rs = ceil(radius * 3);
     float sigma = radius / 2.0;
     float sigmaSquare = sigma * sigma;
     float twoPiSigmaSquare = 2.0 * M_PI * sigmaSquare;
+
+    // Перебор пикселей в области для размытия
     for (auto i = params->startHeight; i < params->endHeight; ++i)
     {
         for (auto j = params->startWidth; j < params->endWidth; ++j)
         {
+            // Инициализация переменных для хранения результатов размытия
             double r = 0, g = 0, b = 0;
             double weightSum = 0;
 
+            // Перебор пикселей в окрестности текущего пикселя
             for (int iy = i - rs; iy < i + rs + 1; ++iy)
             {
                 for (int ix = j - rs; ix < j + rs + 1; ++ix)
                 {
+                    // Обработка граничных случаев, чтобы не выйти за границы изображения
                     auto x = min(static_cast<int>(params->endWidth) - 1, max(0, ix));
                     auto y = min(static_cast<int>(params->endHeight) - 1, max(0, iy));
 
+                    // Вычисление квадрата расстояния между пикселями
                     float distanceSquare = ((ix - j) * (ix - j)) + ((iy - i) * (iy - i));
+                    // Вычисление веса для текущего пикселя
                     float weight = exp(-distanceSquare / (2.0 * sigmaSquare)) / twoPiSigmaSquare;
 
+                    // Получение цветового пикселя из изображения
                     rgb32* pixel = params->in->GetPixel(x, y);
 
+                    // Накопление сумм цветовых компонент с весами
                     r += pixel->r * weight;
                     g += pixel->g * weight;
                     b += pixel->b * weight;
@@ -48,7 +59,10 @@ void Blur(int radius, Params* params)
                 }
             }
 
+            // Получение цветового пикселя из изображения для записи результата
             rgb32* pixel = params->in->GetPixel(j, i);
+
+            // Запись размытого цвета в пиксель
             pixel->r = std::round(r / weightSum);
             pixel->g = std::round(g / weightSum);
             pixel->b = std::round(b / weightSum);
@@ -56,13 +70,15 @@ void Blur(int radius, Params* params)
     }
 }
 
+
 DWORD WINAPI StartThreads(CONST LPVOID lpParam)
 {
-    struct Params* params = (struct Params*)lpParam;
-    Blur(4, params);
-    ExitThread(0);
+    struct Params* params = (struct Params*)lpParam; // Получаем параметры изображения
+    Blur(4, params); // Вызываем функцию размытия
+    ExitThread(0); // Завершаем поток
 }
 
+// Функция StartThreads создает и запускает несколько потоков для обработки изображения
 void StartThreads(Bitmap* in, int threadsCount, int coreCount)
 {
     int partHeight = in->GetHeight() / threadsCount;
@@ -92,13 +108,12 @@ void StartThreads(Bitmap* in, int threadsCount, int coreCount)
         ResumeThread(handles[i]);
     }
 
-
     WaitForMultipleObjects(threadsCount, handles, true, INFINITE);
 
     delete[] arrayParams;
     delete[] handles;
 }
-
+  
 int main(int argc, const char** argv)
 {
     double start = clock();
---
+++ lab3/lab3/lab3.cpp
@@ -0,0 +1,63 @@
+﻿#include <windows.h>
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <ctime>
+#include <vector>
+#pragma comment(lib, "winmm.lib")
+#define _CRT_SECURE_NO_WARNINGS
+
+struct ThreadData {
+    int threadNumber;
+    FILE* outputFile;
+};
+
+DWORD WINAPI ThreadProc(CONST LPVOID lpParam)
+{
+    ThreadData* threadData = static_cast<ThreadData*>(lpParam);
+    int threadNumber = threadData->threadNumber;
+    int operations = 100;
+
+    for (size_t i = 0; i < operations; ++i)
+    {
+        DWORD startTime = timeGetTime();
+
+        Sleep(10);
+
+        DWORD endTime = timeGetTime();
+        double executionTime = endTime - startTime - 10;
+
+        // Записываем время выполнения и номер потока в файл
+        fprintf(threadData->outputFile, "%d|%lf\n", threadNumber, executionTime);
+    }
+
+    ExitThread(0);
+}
+
+int main(int argc, char* argv[])
+{
+    HANDLE handles[2];
+    int threadNumbers[2] = { 1, 2 };
+
+    FILE* outputFile;
+    fopen_s(&outputFile, "output.txt", "w");
+    std::vector<ThreadData> threadData(2);
+    int a;
+    std::cin >> a;
+
+    for (int i = 0; i < 2; ++i)
+    {
+        threadData[i].threadNumber = threadNumbers[i];
+        threadData[i].outputFile = outputFile;
+        handles[i] = CreateThread(NULL, 0, &ThreadProc, &threadData[i], CREATE_SUSPENDED, NULL);
+    }
+
+    for (int i = 0; i < 2; ++i)
+    {
+        ResumeThread(handles[i]);
+    }
+
+    WaitForMultipleObjects(2, handles, TRUE, INFINITE);
+
+    return 0;
+}
